4e02502b96cd41c39780d46b7e6575b1a38f7fbe
Prathamesh <mihirpalyekar75@gmail.com>
2017-01-15 16:19:39 +0530

    Adding Insertion and Bubble sort

---
+++ Bubble/bubble.c
@@ -0,0 +1,16 @@
+void bubble_sort(int List[] , int Limit) {
+    int index1 , index2 ,temporary;
+    for (index1 = 1 ; index1 < Limit ; index1++)
+        for (index2 = 0 ;index2 < Limit-1 ; index2++) {
+            if(List[index2] > List[index2 + 1]) {
+                temporary = List[index2];
+                List[index2] = List[index2 + 1];
+                List[index2 +1] = temporary;
+            }
+    }
+    printf("sorted list is:-");
+    for(index1 = 0 ; index1 < Limit ; index1++) {
+        printf("%d" , List[index1]);
+    }
+    printf("\n");
+}
